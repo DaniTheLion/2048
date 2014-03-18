7e3db05fc0358ffdf3349a3e0c71ad7d88cb5c52
Dani Acherkan <dtl.117@gmail.com>
2014-03-18 22:06:58 +0200

    solver straegy pattern

--- simulator.rb
+++ simulator.rb
@@ -1,20 +1,18 @@
 require './solver'
-NUM_OF_SIMULATIONS = 100
+NUM_OF_SIMULATIONS = 10
 
 res_str = ""
 
 # solver = D2.new
-[ RandomSolver, D1, D2, D3, NeverGoesUp, OneMoveAhead ].each do |solver_class|
-# [ TwoMovesAhead ].each do |solver_class|
+[ RandomSolver, D1, D2, D3, NeverGoesUp, OneMoveAhead ].each do |strategy|
+# [ RandomSolver, D1 ].each do |strategy|
 # [ NeverGoesUp ].each do |solver_class|
-	solver = solver_class.new
+	solver = Solver.new(strategy)
 	n_wins, n_loses = 0, 0
 	max_tiles = []
 	NUM_OF_SIMULATIONS.times do 
 		solver.game = Game.new
-		NUM_OF_SIMULATIONS == 1 ? solver.solve_to_file!("#{solver_class}.log") : solver.solve!
-
-		# solver.solve_to_file!("#{solver_class}.log") if NUM_OF_SIMULATIONS == 1
+		NUM_OF_SIMULATIONS == 1 ? solver.solve_to_file!("#{strategy}.log") : solver.solve!
 		if solver.won?
 			n_wins += 1
 		elsif solver.lost?
@@ -23,6 +21,6 @@ res_str = ""
 		max_tiles << solver.game.board.max_by{|t| t.val}.val
 	end
 	max_avg_tile = max_tiles.inject{ |sum, el| sum + el }.to_f / max_tiles.size
-	res_str += "#{solver.class}\nWON: #{n_wins}\nLOST: #{n_loses}\nHIGHEST TILE: #{max_tiles.max}\nAVERAGE HIGHEST TILE: #{max_avg_tile}\n\n"
+	res_str += "#{solver}\nWON: #{n_wins}\nLOST: #{n_loses}\nHIGHEST TILE: #{max_tiles.max}\nAVERAGE HIGHEST TILE: #{max_avg_tile}\n\n"
 end
 puts res_str
--- solver.rb
+++ solver.rb
@@ -2,17 +2,14 @@ require './2048.rb'
 class Solver
 	attr_accessor :game
 
-	def initialize
+	def initialize(strategy=RandomSolver)
+		@strategy = strategy.new
 		@game = Game.new
 	end
 
 	def solve!
-		# puts @game.board
-		# puts pick_move
 		while !@game.win_or_lose?
-			# puts @game.board
-			# puts pick_move
-			@game.play_single(pick_move)
+			@game.play_single(@strategy.pick_move(@game))
 		end
 	end
 
@@ -21,7 +18,7 @@ class Solver
 		game_log = ""
 		while !@game.win_or_lose?
 			game_log << @game.board.to_s
-			move = pick_move
+			move = @strategy.pick_move(@game)
 			game_log << "#{move}\n\n"
 			@game.play_single(move)
 		end
@@ -37,32 +34,36 @@ class Solver
 		@game.lose?
 	end
 
-	private
-		def pick_move
-			raise "Not Impemented!"
-		end
+	def to_s
+		@strategy.class.to_s
+	end
 end
 
+class Strategy
+	def pick_move(game)
+		raise "Not Implemented!"
+	end
+end
+
+
 # Always chooses randomly
-class RandomSolver < Solver
-	private
-		def pick_move
-			@game.possible_commands.sample	
+class RandomSolver < Strategy
+		def pick_move(game)
+			game.possible_commands.sample	
 		end
 end
 
 # Chosses the move that maximizes a function of the board after the move.
 # Evaludate function is board sum (and it sucks since it prefers not to merge.)
-class D1 < Solver
-	private
+class D1 < Strategy
 		# sum of all tiles
 		def evaluate(game)
 			game.board.inject(0) { |sum, tile| sum + tile.val}
 		end
 
-		def pick_move
-			moves_n_scores = @game.possible_commands.collect do |move|
-				game_copy = Marshal::load(Marshal.dump(@game))
+		def pick_move(game)
+			moves_n_scores = game.possible_commands.collect do |move|
+				game_copy = Marshal::load(Marshal.dump(game))
 				game_copy.simulate_single(move)
 				[move, evaluate(game_copy)]
 			end
@@ -73,7 +74,6 @@ end
 
 
 class D2 < D1
-	private
 		# sum of all tiles, scores higher for more empty tiles and thus choosing to merge
 		def evaluate(game)
 			sum_of_tiles = game.board.inject(0) { |sum, tile| sum + tile.val} 
@@ -86,7 +86,6 @@ end
 # Evaluate function is a sum of all tiles, empty tile factor and a highest tile factor (thus choosing to merge
 # hisgher rather than a few lower merges) 
 class D3 < D1
-	private
 		# sum of all tiles, scores higher for more empty tiles and thus choosing to merge
 		def evaluate(game)
 			sum_of_tiles = game.board.inject(0) { |sum, tile| sum + tile.val} 
@@ -99,11 +98,10 @@ end
 
 # Does not go UP unless no other choice.
 class NeverGoesUp < D1
-	private
-		def pick_move
-			moves = (@game.possible_commands - [:up]).empty? ? @game.possible_commands : @game.possible_commands - [:up]
+		def pick_move(game)
+			moves = (game.possible_commands - [:up]).empty? ? game.possible_commands : game.possible_commands - [:up]
 			moves_n_scores = moves.collect do |move|
-				game_copy = Marshal::load(Marshal.dump(@game))
+				game_copy = Marshal::load(Marshal.dump(game))
 				game_copy.simulate_single(move)
 				[move, evaluate(game_copy)]
 			end
@@ -122,11 +120,10 @@ end
 
 # Maximizes board score two steps ahead
 class OneMoveAhead < D1
-	private
 
-		def pick_move
-			moves_n_scores = @game.possible_commands.collect do |move|
-				game_copy = Marshal::load(Marshal.dump(@game))
+		def pick_move(game)
+			moves_n_scores = game.possible_commands.collect do |move|
+				game_copy = Marshal::load(Marshal.dump(game))
 				game_copy.simulate_single(move)
 				next_move_scores = []
 				game_copy.possible_commands.collect do |next_move|
